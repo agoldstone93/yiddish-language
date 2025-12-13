209b02ab1744dcda1109a4b4457ea771186ca9c4
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2025-12-13 18:15:38 +0000

    initial commit

---
+++ src/lib/verbs.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
+
+const verbsDir = path.join(process.cwd(), "content/verbs");
+
+export function getAllVerbs() {
+  const files = fs.readdirSync(verbsDir);
+
+  return files.map((file) => {
+    const raw = fs.readFileSync(path.join(verbsDir, file), "utf8");
+    return yaml.load(raw) as any;
+  });
+}
