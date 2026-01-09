4785b53ca3aa437fe57f019c8f03a7ab2d66d013
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-01-09 19:15:15 +0000

    Add lastmod to sitemap. Update priority.

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -7,24 +7,28 @@ function generateSiteMap(baseUrl: string) {
   const verbs = getAllVerbs();
   const pages = getAllPages();
   const categories = getAllCategories();
+  const buildDate = new Date().toISOString();
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${baseUrl}</loc>
+    <lastmod>${buildDate}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>${baseUrl}/verbs</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.9</priority>
+    <lastmod>${buildDate}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
   </url>
   ${pages
     .map(
       (page) => `
   <url>
     <loc>${baseUrl}/${page.slug}</loc>
+    <lastmod>${buildDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>`
@@ -35,6 +39,7 @@ function generateSiteMap(baseUrl: string) {
       (cat) => `
   <url>
     <loc>${baseUrl}/categories/${cat.id}</loc>
+    <lastmod>${buildDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>`
@@ -45,6 +50,7 @@ function generateSiteMap(baseUrl: string) {
       (verb) => `
   <url>
     <loc>${baseUrl}/verbs/${verb.id}</loc>
+    <lastmod>${buildDate}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.6</priority>
   </url>`
@@ -53,17 +59,13 @@ function generateSiteMap(baseUrl: string) {
 </urlset>`;
 }
 
-function SiteMap() {
-  // getServerSideProps will do the work
-}
-
-export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
-  const protocol = req.headers.host?.includes('localhost') ? 'http' : 'https';
-  const baseUrl = `${protocol}://${req.headers.host}`;
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = 'https://loshnlab.com';
   
   const sitemap = generateSiteMap(baseUrl);
 
   res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Cache-Control', 'public, s-maxage=86400, stale-while-revalidate');
   res.write(sitemap);
   res.end();
 
@@ -72,4 +74,4 @@ export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
   };
 };
 
-export default SiteMap;
+export default function SiteMap() {}
