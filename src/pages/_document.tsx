b47ff6487534acb770df2ed4ec7dbc019ff04b3b
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2026-03-21 19:41:02 +0000

    Add opengraph image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#111827" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta property="og:title" content="LoshnLab" />
+        <meta property="og:image" content="https://loshnlab.com/icons/opengraph-img.png" />
+        <meta property="og:type" content="website" />
       </Head>
       <body className="antialiased">
         <Main />
